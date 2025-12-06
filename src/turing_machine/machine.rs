95e1838c772e4c9d53b072eedd69d843762bb25c
3yassu <68251430+3yassu@users.noreply.github.com>
2025-12-05 23:54:16 -0600

    Added comments

--- src/turing_machine/machine.rs
+++ src/turing_machine/machine.rs
@@ -1,6 +1,8 @@
 //turing_machine/machine.rs
+//bring the Tape and pointer to Cell in scope as well as States
 use super::tape::{Tape, CellPtr};
 use super::states::State;
+//A turing machine is the Tape, the current position in the tape, and the current State of the machine
 #[allow(unused)]
 pub struct CTuringMachine{
 	tape: Tape,
@@ -8,12 +10,15 @@ pub struct CTuringMachine{
 	cur_state: State
 }
 impl CTuringMachine{
+	//creates a new turing machine given a tape (starts at front with start state)
 	pub fn new(tape: Tape) -> Self{
 		Self{cur: tape.peek_front_node(), tape, cur_state: State::S}
 	}
+	//Read char, represents a state transition
 	fn read_char(&mut self){
 		self.cur_state.read_char(&mut self.cur)
 	}
+	//Reads char until a final state is reaches
 	pub fn run(&mut self) -> bool {
 		while(self.cur_state != State::A) && (self.cur_state != State::F){
 			self.read_char();
