4ee08cd7d58416e314293cab4eaaae8cfd68f7d6
3yassu <68251430+3yassu@users.noreply.github.com>
2025-12-05 23:59:58 -0600

    Update terminals.rs

--- src/turing_machine/terminals.rs
+++ src/turing_machine/terminals.rs
@@ -1,5 +1,7 @@
 //turing_machine/terminals.rs
+//needed for function
 use std::mem;
+//TerminalChar is an enum of all possible TerminalCharacters
 #[allow(unused)]
 pub enum TerminalChar{
 	ParenOpen,
@@ -14,6 +16,7 @@ pub enum TerminalChar{
 	Char(char),
 }
 impl TerminalChar{
+	//Makes it so clone is never requires, sneaky little trick
     pub const fn take(&mut self) -> Self {
 		//Replace with mem::take with latter is const ready
         mem::replace(self, TerminalChar::Blank)
